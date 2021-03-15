28b05301fccd0798019021df4003ff05f1d1e79b
Timothé Gonin <gonin.timothe@gmail.com>
2021-03-15 17:06:15 +0100

    bdd files  / css->error msg
    if password = password_confirm
    req for if email exist
    file for require/bdd connection

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+    require('src/connection.php');
+
+    if(!empty($_POST['pseudo']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['password_confirm'])){
+        
+        //VARIABLES
+        $pseudo = $_POST['pseudo'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $pass_confirm = $_POST['password_confirm'];
+
+        //IF PASSWORD = PASSWORD_CONFIRM
+        if($password != $pass_confirm){
+            header('location: index.php?error=1&pass=1');
+        };
+
+        //IF EMAIL EXIST
+        $req = $db->prepare('SELECT count(*) as numberEmail FROM users WHERE email= ?');
+        $req->execute(array($email));
+    }
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -23,6 +44,12 @@
                     </tr>
                 </thead>
                 <tbody>
+<?php
+    if(isset($_GET['error'])){
+        if(isset($_GET['pass']))
+        echo '<p class="error-msg box-center txt-center">Les mots de pass ne sont pas identiques.</p>';
+    }
+?>
                     <form action="index.php" method="post">
                         <tr  class="txt-center">
                             <td>
@@ -41,22 +68,23 @@
                         </tr>
                         <tr  class="txt-center">
                             <td>
-                                <input type="password" name="password" class="form-class" placeholder="******" required>
+                                <input type="password" name="password_confirm" class="form-class" placeholder="******" required>
                             </td>
                         </tr>
-                    </form>
+                    
                 </tbody>
                 <tfoot>
                     <tr  class="txt-center">
                         <td>
                         <button type="submit" id="signInBtn" name="singin" class="box-center btn sign-btn-mid pointer">Sign in</button>
                         </td>
+                    </form>
                     </tr>
 <!-- btn separation --><tr><td><hr></td></tr>
                     <tr  class="txt-center">
                         <td>
                             <p id="signInProposition">Want to log in ?</p>
-                            <a href="#"><button type="submit" id="logInBtn" name="login" class="box-center btn log-btn-sml pointer">Log in</button></a>
+                            <!-- <a href="#"><button type="submit" id="logInBtn" name="login" class="box-center btn log-btn-sml pointer">Log in</button></a> -->
                         </td>
                     </tr>
                 </tfoot>    
