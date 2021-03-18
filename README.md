4c3bdc91b70a408134d3262d3944113e523783b2
Timothé Gonin <gonin.timothe@gmail.com>
2021-03-18 08:52:59 +0100

    bdd file, caution, logs modify for forks

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-Members-area-access
+# Members-area-access
+MEMBERS AREA ACCESS
+
+## Description 
+Users can sign in, log in, log out.
+Passwords are encrypted with "my salt" 
+
+## Caution :
+[LOCAL]
+It is necessary to send the file [formation_members.sql], located in the directory [mysql], to your database.
+
+Then you will need to modify the file [connection], located in the directory [src], entering your own username and password for your database connexion.
+
+## Tech Used : 
+* PHP
+* HTML
+* CSS
+
---
+++ mysql/formation_members.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 18 mars 2021 à 07:34
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `formation_members`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `secret` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `pseudo`, `email`, `password`, `creation_date`, `secret`) VALUES
+(2, 'Php', 'php@php.php', 'aq150fba74b164c214973c86c0b8082ba2b52ad466d25', '2021-03-16 15:29:15', '867a79312657b925d81fcbd6f10ec1118c179fc716159049551615904955');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
